9c45943f15345b5d1a6918d90ee32e0f04f51a7a
calKU0 <228528@student.uek.krakow.pl>
2025-10-22 15:35:10 +0200

    change order status to ended when stock <=1

--- GaskaAllegroProductsSyncService/Helpers/OfferFactory.cs
+++ GaskaAllegroProductsSyncService/Helpers/OfferFactory.cs
@@ -103,7 +103,7 @@ namespace AllegroGaskaProductsSyncService.Helpers
                 },
                 Publication = new Publication
                 {
-                    Status = offer.Product.InStock > 0 ? "ACTIVE" : "ENDED",
+                    Status = offer.Product.InStock > 1 ? "ACTIVE" : "ENDED",
                     StartingAt = null,
                 },
                 Delivery = new Delivery
