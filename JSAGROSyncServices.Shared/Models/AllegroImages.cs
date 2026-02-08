1245886731207077a52411354bf794ec9ac39e77
Krzysztof Kurowski <krzysztop497@gmail.com>
2026-02-08 23:22:06 +0100

    refactor: add new field

--- JSAGROSyncServices.Shared/Models/AllegroImages.cs
+++ JSAGROSyncServices.Shared/Models/AllegroImages.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using JSAGROSyncServices.Shared.Data.Enums;
 
 namespace JSAGROSyncServices.Shared.Models
 {
@@ -9,6 +7,7 @@ namespace JSAGROSyncServices.Shared.Models
         public int Id { get; set; }
         public int ProductId { get; set; }
         public string Url { get; set; }
+        public AllegroAccount Account { get; set; }
         public bool Connected { get; set; } = false;
     }
 }
